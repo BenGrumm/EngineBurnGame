30be1bde7d44f081ce3cb2a8718093f9e9247961
Sectioned <Sectioned@users.noreply.github.com>
2018-08-31 18:48:55 +0100

    ok

--- EngineBurn/app/src/main/java/com/bgrummitt/engineburn/activities/character_select/CharacterAdapter.java
+++ EngineBurn/app/src/main/java/com/bgrummitt/engineburn/activities/character_select/CharacterAdapter.java
@@ -1,6 +1,7 @@
 package com.bgrummitt.engineburn.activities.character_select;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -19,18 +20,19 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
 
     private Context mContext;
     private Characters[] mCharacterList;
+    private String highlightedCharacter;
+
 
     public CharacterAdapter(Context context, Characters[] characters){
         mContext = context;
         mCharacterList = characters;
-
-        CharacterSelectionActivity CharacterActivity = (CharacterSelectionActivity) context;
     }
 
     public class CharacterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public String name;
         public ImageView mCharacterImage;
+        public boolean isHighlighted = false;
 
         public CharacterViewHolder(View itemView){
             super(itemView);
@@ -48,15 +50,21 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.Char
         @Override
         public void onClick(View v) {
             Log.v(TAG, "Clicked " + name);
+            HighlightObject(v);
         }
+
+        public void HighlightObject(View v){
+            v.setBackgroundColor(!isHighlighted ? 0x44FFFFFF : 0x00FFFFFF);
+            isHighlighted = !isHighlighted;
+        }
+
     }
 
     @NonNull
     @Override
     public CharacterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.character_list_item, parent, false);
-        CharacterViewHolder viewHolder = new CharacterViewHolder(view);
-        return viewHolder;
+        return new CharacterViewHolder(view);
     }
 
     @Override
